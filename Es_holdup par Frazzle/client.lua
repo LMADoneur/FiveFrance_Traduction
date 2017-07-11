f9f737b3ffbbe6ffa085c0049be6c59851b852a1
Marceau <marceau.bernelin@gmail.com>
2017-07-11 14:48:00 +0200

    es_holdup traduit

--- Es_holdup par Frazzle/client.lua
+++ Es_holdup par Frazzle/client.lua
@@ -28,25 +28,25 @@ local stores = {
 	["paleto_twentyfourseven"] = {
 		position = { ['x'] = 1730.35949707031, ['y'] = 6416.7001953125, ['z'] = 35.0372161865234 },
 		reward = 5000,
-		nameofstore = "24/7 (Paleto Bay)",
+		nameofstore = "24/7 (La baie de Paleto)",
 		lastrobbed = 0
 	},
 	["sandyshores_twentyfoursever"] = {
 		position = { ['x'] = 1960.4197998047, ['y'] = 3742.9755859375, ['z'] = 32.343738555908 },
 		reward = 5000,
-		nameofstore = "24/7 (Sandy Shores)",
+		nameofstore = "24/7 (Les rives de Sandy)",
 		lastrobbed = 0
 	},
 	["bar_one"] = {
 		position = { ['x'] = 1986.1240234375, ['y'] = 3053.8747558594, ['z'] = 47.215171813965 },
 		reward = 5000,
-		nameofstore = "Yellow Jack. (Sandy Shores)",
+		nameofstore = "Yellow Jack. (Les rives de Sandy)",
 		lastrobbed = 0
 	},
 	["littleseoul_twentyfourseven"] = {
 		position = { ['x'] = -709.17022705078, ['y'] = -904.21722412109, ['z'] = 19.215591430664 },
 		reward = 5000,
-		nameofstore = "24/7 (Little Seoul)",
+		nameofstore = "24/7 (Petite Seoul)",
 		lastrobbed = 0
 	}
 }
@@ -61,7 +61,7 @@ end)
 RegisterNetEvent('es_holdup:toofarlocal')
 AddEventHandler('es_holdup:toofarlocal', function(robb)
 	holdingup = false
-	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "The robbery was cancelled, you will receive nothing.")
+	TriggerEvent('chatMessage', 'SYSTÈME', {255, 0, 0}, "Le braquage à été annulé, vous n'allez rien recevoir.")
 	robbingName = ""
 	secondsRemaining = 0
 	incircle = false
@@ -71,7 +71,7 @@ end)
 RegisterNetEvent('es_holdup:robberycomplete')
 AddEventHandler('es_holdup:robberycomplete', function(robb)
 	holdingup = false
-	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "Robbery done, you received: ^2" .. stores[store].reward)
+	TriggerEvent('chatMessage', 'SYSTÈME', {255, 0, 0}, "Braquare réussi, vous avez reçu: ^2" .. stores[store].reward)
 	store = ""
 	secondsRemaining = 0
 	incircle = false
@@ -99,7 +99,7 @@ Citizen.CreateThread(function()
 		SetBlipScale(blip, 0.8)
 		SetBlipAsShortRange(blip, true)
 		BeginTextCommandSetBlipName("STRING")
-		AddTextComponentString("Robbable Store")
+		AddTextComponentString("Magasins braquable")
 		EndTextCommandSetBlipName(blip)
 	end
 end)
@@ -118,7 +118,7 @@ Citizen.CreateThread(function()
 					
 					if(Vdist(pos.x, pos.y, pos.z, pos2.x, pos2.y, pos2.z) < 1.0)then
 						if (incircle == false) then
-							DisplayHelpText("Press ~INPUT_CONTEXT~ to rob ~b~" .. v.nameofstore .. "~w~ beware, the police will be alerted!")
+							DisplayHelpText("Appuie sur ~INPUT_CONTEXT~ pour braquer ~b~" .. v.nameofstore .. "~w~ attention, la police va être alerté!")
 						end
 						incircle = true
 						if(IsControlJustReleased(1, 51))then
@@ -133,7 +133,7 @@ Citizen.CreateThread(function()
 
 		if holdingup then
 
-			drawTxt(0.66, 1.44, 1.0,1.0,0.4, "Robbing store: ~r~" .. secondsRemaining .. "~w~ seconds remaining", 255, 255, 255, 255)
+			drawTxt(0.66, 1.44, 1.0,1.0,0.4, "Braquage du magasin: ~r~" .. secondsRemaining .. "~w~ secondes restantes", 255, 255, 255, 255)
 			
 			local pos2 = stores[store].position
 
